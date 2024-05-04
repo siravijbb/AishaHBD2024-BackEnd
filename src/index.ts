d36b36279dfd3cca46085c4cdfd206fc65739f54
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-04 10:15:10 +0700

    add swagger and cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app =
+    new Elysia()
+        .use(cors({
+            origin: /.*\.polygang\.fan$/ || "http://localhost:5173",
+        }))
+        .use(swagger())
+        .get("/", () => "Hello Elysia").listen(3000);
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
